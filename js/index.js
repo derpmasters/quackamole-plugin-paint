8bac05630b92c16859dc3d02ff83608008ec1658
Galvandi <andreas_schoch@outlook.com>
2020-05-16 16:14:18 +0200

    initial

---
+++ js/index.js
@@ -0,0 +1,22 @@
+const output = document.getElementById('output');
+const btnSendRandom = document.getElementById('send-random');
+
+const getRandomNumber = () => {
+    return Math.round(Math.random() * 1000);
+};
+
+// send message from iframe ===> parent
+btnSendRandom.addEventListener('click', () => {
+   console.log('sending random number');
+   const randomNumber = '' + getRandomNumber();
+    output.innerText = randomNumber;
+    window.top.postMessage(randomNumber, '*');
+});
+
+// receive messages from parent ===> iframe
+window.addEventListener('message', (event) => {
+    if (event.origin === 'localhost:3000') {
+        output.innerText = event.data;
+        console.log('message received', event);
+    }
+});
