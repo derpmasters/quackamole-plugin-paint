1183b477cb15806f992eace51c7d11fa7a2d07d1
Galvandi <andreas_schoch@outlook.com>
2020-06-07 21:35:25 +0200

    started using experimental plugin sdk

--- js/index.js
+++ js/index.js
@@ -1,27 +1,16 @@
 const output = document.getElementById('output');
 const btnSendRandom = document.getElementById('send-random');
+const quackamole = new Quackamole();
 
 const getRandomNumber = () => {
     return Math.round(Math.random() * 1000);
 };
 
-// send message from iframe ===> parent
 btnSendRandom.addEventListener('click', () => {
-   console.log('sending random number');
-   const randomNumber = getRandomNumber();
-   const data = {
-       type: 'broadcast',
-       payload: {randomNumber}
-   };
-    output.innerText = '' + randomNumber;
-    window.top.postMessage(data, '*');
+   const data = {randomNumber: getRandomNumber()};
+    quackamole.broadcastData('RANDOM_NUMBER', data);
 });
 
-// receive messages from parent ===> iframe
-window.addEventListener('message', (event) => {
-    console.log('plugin received message', event);
-    if (event.data.type === 'pluginData') {
-        output.innerText = '' + event.data.payload.randomNumber;
-        console.log('random number received', event);
-    }
+quackamole.eventManager.on('RANDOM_NUMBER', ({randomNumber}) => {
+    output.innerText = '' + payload.randomNumber;
 });
